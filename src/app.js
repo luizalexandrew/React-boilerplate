0d8e05a34fe0051f27c23b4209f0fce6e6a307f1
Luiz Alexandre <luiz.alexandre@live.com>
2017-01-06 15:06:32 -0200

    init boilerplate

---
+++ src/app.js
@@ -0,0 +1,13 @@
+'use strict'
+
+import React from 'react'
+
+const App = React.createClass({
+  render: function () {
+    return <h1>React é *****!!!</h1>
+  }
+})
+
+//  samestandard outro lint
+
+export default App
